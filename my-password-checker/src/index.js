c4d19b75c52710f442d576d1a12b185b54e60de4
E_Mithil <158134822+EMITHIL-CBIT@users.noreply.github.com>
2024-10-25 09:19:59 +0530

    Add files via upload

---
+++ my-password-checker/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import PasswordStrengthChecker from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <PasswordStrengthChecker />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
